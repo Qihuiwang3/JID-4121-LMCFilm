558bfeedc901d6ec2ce0273b0bd99b2828b84a9f
mengzhuou <mou30@gatech.edu>
2024-08-22 22:08:01 -0400

    added backend html page with css

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3500;
 const cors = require('cors');
 
 app.use('/', express.static(path.join(__dirname, '/public')));
-// app.use("/", require("./routes/root"));
+app.use("/", require("./routes/root"));
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
 // const mongoose = require("mongoose");
