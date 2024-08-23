f1b83a5ee026ba996636198bcca54af4bc03d24c
mengzhuou <mou30@gatech.edu>
2024-08-22 22:05:47 -0400

    added basic packages

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+const cors = require('cors');
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+// app.use("/", require("./routes/root"));
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+// const mongoose = require("mongoose");
+
+// require("dotenv").config();
+
+
+// app.use(cors());
+// app.use(express.json());
+
+
+
+// // console.log(process.env.NODE_ENV);
+
+// const uri = process.env.DATABASE_URI;
+
+// mongoose.connect(
+//     uri,
+//     { 
+//         useNewUrlParser: true, 
+//         useCreateIndex: true 
+//     }
+// );
+
+// // mongoose.connection.on("error", (err) => {
+// //     console.log(err);
+
+// // });
+
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//     console.log("You are connected to MongoDB");
+// });
