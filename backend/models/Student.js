b749cac5621b0f64d40276f2af02e578e74fbdfa
mengzhuou <mou30@gatech.edu>
2024-08-26 18:47:18 -0400

    update student api

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose')
 
 const studentSchema = new mongoose.Schema({
+    classCode: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    cellPhone: {
+        type: Number,
         required: true
     },
     role: {
