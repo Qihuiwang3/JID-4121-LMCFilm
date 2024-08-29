d34aeb697ca358381ed71e8cd668d9d0f7472503
Xisheng0725 <xzz5367@gmail.com>
2024-08-29 11:36:49 -0400

    get all class code for testing

--- backend/routes/classCodeRoutes.js
+++ backend/routes/classCodeRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.post('/class-code', createClassCode);
 router.get('/class-code/:code', getClassCode);
+// Route to get all class codes
+router.get('/class-codes', getAllClassCodes);
 module.exports = router;
