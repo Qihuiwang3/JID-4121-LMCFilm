26b7f9e45967a7171381e28221be4909ad8868e3
weswilson24 <weswilson2424@gmail.com>
2024-09-08 22:54:05 +0000

    Scrum 97 Forgot to Update Backend was in frontEnd Directory when Commited

--- backend/routes/cartsRoutes.js
+++ backend/routes/cartsRoutes.js
@@ -5,5 +5,8 @@ const limiter = require('../middleware/rateLimiter');
 
 router.get('/', limiter, cartsController.getCart);
 router.post('/', limiter, cartsController.createCart);
+router.put('/carts/:itemId', limiter, cartsController.updateCart);
+router.delete('/carts/:itemId', limiter, cartsController.deleteCart);
+
 
 module.exports = router;
