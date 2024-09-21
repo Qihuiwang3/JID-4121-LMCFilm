023873082d305ec23c50261bb9ee813bfc0f068c
mengzhuou <mou30@gatech.edu>
2024-09-21 17:15:57 -0400

    adjusted scroll bar and table height

--- frontend/src/components/Functions/AgGridTable/AgGridTable.js
+++ frontend/src/components/Functions/AgGridTable/AgGridTable.js
@@ -10,7 +10,7 @@ class AgGridTable extends Component {
 
         return (
             <div className="body">
-                <div className="RecordPageContainer ag-theme-alpine" style={{ width: '50%' }}>
+                <div className="page-container ag-theme-alpine" style={{ width: '50%' }}>
                     <AgGridReact
                         rowData={rowData}
                         columnDefs={columnDefs}
