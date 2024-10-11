26483b856aedfc6977dcbd017d58e9e0ac9781cb
denial <danpan1232@gmail.com>
2024-10-11 15:08:56 -0400

    Reservation history main page and css

---
+++ frontend/src/components/Pages/ReservationHistory/ReservationHistory.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function ReservationHistory() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ReservationHistory;
