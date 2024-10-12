e01c197a92590a83a8a9990f2d3964dba42af873
denial <danpan1232@gmail.com>
2024-10-12 00:56:16 -0400

    Revert "Uncomment create order api and add account details"
    This reverts commit 86feec28df9a02f51e8eb5b80e76b5a86b40d172.

--- frontend/src/components/Pages/ReservationConfirmationMessagePage/ReservationConfirmationMessagePage.js
+++ frontend/src/components/Pages/ReservationConfirmationMessagePage/ReservationConfirmationMessagePage.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './ReservationConfirmationMessagePage.css';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-
+import { useNavigate } from 'react-router-dom';
 import Barcode from 'react-barcode';
 import emailjs from 'emailjs-com';
 import Button from '../../Button/Button';
@@ -10,63 +8,45 @@ import Button from '../../Button/Button';
 function ReservationConfirmationMessagePage() {
     const [orderNumber, setOrderNumber] = useState('');
     const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const location = useLocation();
-
-    const reduxStudentInfo = useSelector(state => state.studentData);
-    const studentInfo = location.state?.studentInfo || reduxStudentInfo;
-
-    const selectedDates = useSelector(state => state.classData.selectedDates);
-
-    const [pickupDateTime, setPickupDateTime] = useState(new Date(selectedDates?.pickupDateTime || new Date()));
-    const [returnDateTime, setReturnDateTime] = useState(new Date(selectedDates?.returnDateTime || new Date()));
-
 
     const generateOrderNumber = () => {
         return 'Order-' + Math.floor(Math.random() * 1000000000);
     };
 
+    // const createOrder = async () => {
+    //     const generatedOrderNumber = generateOrderNumber();
+    //     setOrderNumber(generatedOrderNumber);
 
+    //     const orderData = {
+    //         orderNumber: generatedOrderNumber,
+    //         email,
+    //         checkin,
+    //         checkout,
+    //         studentName
+    //     };
+
+    //     try {
+    //         const response = await fetch('http://localhost:3500/api/order', {
+    //             method: 'POST',
+    //             headers: { 'Content-Type': 'application/json' },
+    //             body: JSON.stringify(orderData)
+    //         });
+    //         const data = await response.json();
+    //         if (response.ok) {
+    //             console.log('Order created successfully:', data);
+    //         } else {
+    //             console.error('Error creating order:', data.error);
+    //         }
+    //     } catch (error) {
+    //         console.error('Error submitting order:', error);
+    //     }
+    // };
 
     useEffect(() => {
-
         const generatedOrderNumber = generateOrderNumber();
         setOrderNumber(generatedOrderNumber);
-
-        const createOrder = async () => {
-            const generatedOrderNumber = generateOrderNumber();
-            setOrderNumber(generatedOrderNumber);
-
-            const orderData = {
-                orderNumber: generatedOrderNumber,
-                email: studentInfo.email,
-                checkin: pickupDateTime,
-                checkout: returnDateTime,
-                studentName: studentInfo.name
-            };
-
-            console.log(orderData);
-
-            try {
-                const response = await fetch('http://localhost:3500/api/order', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(orderData)
-                });
-                const data = await response.json();
-                if (response.ok) {
-                    console.log('Order created successfully:', data);
-                } else {
-                    console.error('Error creating order:', data.error);
-                }
-            } catch (error) {
-                console.error('Error submitting order:', error);
-            }
-        };
-
-        createOrder();
-
-    }, [pickupDateTime, returnDateTime, studentInfo, studentInfo.email, studentInfo.name, setOrderNumber]);
+        // createOrder();
+    }, []);
 
     // Function to handle the back button click
     const goBack = () => {
@@ -77,7 +57,6 @@ function ReservationConfirmationMessagePage() {
         <div className="main-content">
             <h1 className='select-class-header'>
                 Your Reservation has been confirmed!
-
             </h1>
             <div className='confirm-text'>
                 Here is your QR Code for pickup and return purposes. You can also find this QR code under Profile.
