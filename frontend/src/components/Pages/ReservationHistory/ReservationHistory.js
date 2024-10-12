a229379eb0c96cec8947de7a8f527dce8b9eff10
denial <danpan1232@gmail.com>
2024-10-12 01:20:44 -0400

    Test page i dunno

--- frontend/src/components/Pages/ReservationHistory/ReservationHistory.js
+++ frontend/src/components/Pages/ReservationHistory/ReservationHistory.js
@@ -3,6 +3,27 @@ import React, { useState, useEffect } from 'react';
 function ReservationHistory() {
     return (
         <div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
+            <div>
+                SOHOS
+            </div>
 
         </div>
     )
