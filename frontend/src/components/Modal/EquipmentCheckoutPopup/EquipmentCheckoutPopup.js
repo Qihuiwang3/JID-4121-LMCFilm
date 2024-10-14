8b8bce2bc9d506c73b7efe03dda1e34a67dd1d87
mengzhuou <mou30@gatech.edu>
2024-10-13 21:32:14 -0400

    Added cancel and refine checkotpopup css

--- frontend/src/components/Modal/EquipmentCheckoutPopup/EquipmentCheckoutPopup.js
+++ frontend/src/components/Modal/EquipmentCheckoutPopup/EquipmentCheckoutPopup.js
@@ -6,18 +6,26 @@ const EquipmentCheckoutPopup = ({ onClose }) => {
         <div className="equipment-checkout-overlay" onClick={onClose}>
             <div className="equipment-checkout-content" onClick={(e) => e.stopPropagation()}>
                 <h2>Scan</h2>
+                <button
+                        className="close-button"
+                        onClick={onClose}
+                    >
+                        <div className='close-button-inner'>
+                            &times;
+                        </div>
+                </button>
                 <div className="equipment-checkout-input">
                     <label htmlFor="barcode">Bar Code</label>
-                    <input type="text" id="barcode" placeholder="Scan or enter Bar Code" />
+                    <input type="text" id="barcode" className="checkout-modal-input"/>
                 </div>
                 
                 <div className="equipment-checkout-input">
                     <label htmlFor="email">Student Email</label>
-                    <input type="email" id="email" placeholder="Enter Student Email" />
+                    <input type="email" id="email" className="checkout-modal-input"/>
                 </div>
                 
-                <div className="equipment-checkout-buttons">
-                    <button className="cancel-button" onClick={onClose}>Cancel</button>
+                <div className="modal-footer">
+                    <button className="cancelModal-button" onClick={onClose}>Cancel</button>
                     <button className="scan-search-button">Search</button>
                 </div>
             </div>
