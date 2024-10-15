bca30b0eaaead780207a2be6afa3186964d0301b
weswilson24 <weswilson2424@gmail.com>
2024-10-15 16:52:19 -0400

    Create BarCodePopup.js
    Pop up modal for the bar code

---
+++ frontend/src/components/BarCodePopup/BarCodePopup.js
@@ -0,0 +1,56 @@
+
+import React, { useState, useEffect } from 'react';
+import './BarCodePopup.css';
+import Barcode from 'react-barcode';
+import { getOrderByOrderNumber } from '../../../connector.js';
+
+
+const BarCodePopup = ({ show, handleClose, orderNumber}) => {
+    const [orderNum, setOrderNumber] = useState('');
+   
+    useEffect(() => {
+        const fetchOrders= async () => {
+            try {
+                const num = await getOrderByOrderNumber(orderNumber);
+                setOrderNumber(num);
+            } catch (error) {
+               
+            }
+        };
+        fetchOrders();
+    }, []);
+
+
+    if (!show) {
+        return null;
+    }
+
+return (
+
+<div className="modal-overlay">
+    <div className="modal-content">
+        <div className="modal-header">
+            <h2>View Damage Report</h2>
+            <button className="close-button" onClick={handleClose}>×</button>
+        </div>
+        <div className="report-details">
+            <p>Please show the attached QR code to the staff member when you arrive. When returning equipment after use, please show this QR code again.</p>
+
+            <div className="barcode-container">
+                <Barcode value={orderNum} />
+            </div>
+        </div>
+        <div className="modal-footer">
+            <button className="cancelModal-button" onClick={handleClose}>Cancel</button>
+            <button className="saveModal-button" onClick={handleClose}>Done</button>
+        </div>
+    </div>
+</div>
+
+
+
+);
+
+}
+
+export default BarCodePopup;
