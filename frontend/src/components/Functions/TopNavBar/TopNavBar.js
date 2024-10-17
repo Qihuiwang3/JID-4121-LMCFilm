fd097f7dbdecf67f0b293850e41553ab5d457cc5
Qihui <qwang@Qihuis-Mac-mini.local>
2024-10-17 12:37:05 -0400

    my profile modal

--- frontend/src/components/Functions/TopNavBar/TopNavBar.js
+++ frontend/src/components/Functions/TopNavBar/TopNavBar.js
@@ -1,67 +1,61 @@
-import React, { Component } from "react";
-import { withFuncProps } from "../../withFuncProps";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import './TopNavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
+import ProfileModal from "../../Modal/ProfileModal/ProfileModal";
 
+const TopNavBar = () => {
+    const [isDroppedDown, setIsDroppedDown] = useState(false);
+    const [showProfileModal, setShowProfileModal] = useState(false);
+    const navigate = useNavigate();
+    const studentData = useSelector((state) => state.studentData);
 
+    const toggleDropdown = () => {
+        setIsDroppedDown(!isDroppedDown);
+    };
 
-class TopNavBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isDroppedDown: false,
-        };
-    }
+    const openProfileModal = () => {
+        setShowProfileModal(true);
+    };
 
-    toggleDropdown = () => {
-        const { isDroppedDown } = this.state;
-        this.setState({isDroppedDown: !isDroppedDown})
-    }
-    
-    // profileNav = () => {
-    //     this.props.navigate("/Profile");
-    // }
+    const closeProfileModal = () => {
+        setShowProfileModal(false);
+    };
 
-    // notificationeNav = () => {
-    //     this.props.navigate("/Notification");
-    // }
+    const logoutNav = () => {
+        navigate("/");
+        setIsDroppedDown(false);
+    };
 
-    logoutNav = () => {
-        // goes to EnterCode page for now
-        this.props.navigate("/");
-        const { isDroppedDown } = this.state;
-        this.setState({isDroppedDown: !isDroppedDown})
-    }
-
-
-
-    render() {
-        const { isDroppedDown } = this.state;
-        return (
-            <div className="navBar">
-                <div className="navBar-left">
-                    <div className="navTitle">LMC Film</div>
+    return (
+        <div className="navBar">
+            <div className="navBar-left">
+                <div className="navTitle">LMC Film</div>
+            </div>
+            <div className="navBar-right">
+                <div className="userIcon" onClick={toggleDropdown}>
+                    <FontAwesomeIcon icon={faCircleUser} />
                 </div>
-                <div className="navBar-right">
-                    <div className="userIcon" onClick={this.toggleDropdown}>
-                        <FontAwesomeIcon icon={faCircleUser} />
-                    </div>
-                    <div className="dropdownIcon">
-                        <FontAwesomeIcon icon={isDroppedDown? faAngleUp: faAngleDown} onClick={this.toggleDropdown}/>
-                        {isDroppedDown && (
-                            <div className="dropdown-container">
-                                <div className="dropdown-content">Profile</div>
-                                <div className="dropdown-content" onClick={this.logoutNav}>Logout</div>
-                            </div>                        
-                        )}
-
-                    </div>
+                <div className="dropdownIcon">
+                    <FontAwesomeIcon icon={isDroppedDown ? faAngleUp : faAngleDown} onClick={toggleDropdown} />
+                    {isDroppedDown && (
+                        <div className="dropdown-container">
+                            <div className="dropdown-content" onClick={openProfileModal}>Profile</div>
+                            <div className="dropdown-content" onClick={logoutNav}>Logout</div>
+                        </div>
+                    )}
                 </div>
             </div>
-        );
-    }
-}
+            <ProfileModal 
+                show={showProfileModal}
+                onClose={closeProfileModal}
+                studentData={studentData}
+            />
+        </div>
+    );
+};
 
-export default withFuncProps(TopNavBar);
+export default TopNavBar;
