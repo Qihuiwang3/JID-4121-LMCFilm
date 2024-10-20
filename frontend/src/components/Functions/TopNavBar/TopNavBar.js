ccfd1f67483fbc53344d1b0be54c7419a2d8c4a2
Qihui Wang <qwang491@gatech.edu>
2024-10-20 16:08:55 -0400

    new nav bar

--- frontend/src/components/Functions/TopNavBar/TopNavBar.js
+++ frontend/src/components/Functions/TopNavBar/TopNavBar.js
@@ -3,22 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import './TopNavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
-import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons';
 import ProfileModal from "../../Modal/ProfileModal/ProfileModal";
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const TopNavBar = () => {
-    const [isDroppedDown, setIsDroppedDown] = useState(false);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [showProfileModal, setShowProfileModal] = useState(false);
     const navigate = useNavigate();
     const studentData = useSelector((state) => state.studentData);
 
-    const toggleDropdown = () => {
-        setIsDroppedDown(!isDroppedDown);
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
     };
 
     const openProfileModal = () => {
         setShowProfileModal(true);
+        setIsSidebarOpen(false);
     };
 
     const closeProfileModal = () => {
@@ -27,7 +27,7 @@ const TopNavBar = () => {
 
     const logoutNav = () => {
         navigate("/");
-        setIsDroppedDown(false);
+        setIsSidebarOpen(false);
     };
 
     return (
@@ -36,20 +36,26 @@ const TopNavBar = () => {
                 <div className="navTitle">LMC Film</div>
             </div>
             <div className="navBar-right">
-                <div className="userIcon" onClick={toggleDropdown}>
-                    <FontAwesomeIcon icon={faCircleUser} />
+                <div
+                    className={`hamburgerIcon ${isSidebarOpen ? 'hamburgerIcon-shifted' : ''}`}
+                    onClick={toggleSidebar}
+                >
+                    <FontAwesomeIcon icon={isSidebarOpen ? faTimes : faBars} />
                 </div>
-                <div className="dropdownIcon">
-                    <FontAwesomeIcon icon={isDroppedDown ? faAngleUp : faAngleDown} onClick={toggleDropdown} />
-                    {isDroppedDown && (
-                        <div className="dropdown-container">
-                            <div className="dropdown-content" onClick={openProfileModal}>Profile</div>
-                            <div className="dropdown-content" onClick={logoutNav}>Logout</div>
+                <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                    <div className="profile-section">
+                        <div className="profile-info">
+                            <h3 className="student-name-bar">{studentData.name}</h3>
                         </div>
-                    )}
+                    </div>
+                    <div className="nav-links">
+                        <div className="nav-link" onClick={openProfileModal}>Profile</div>
+                        <div className="nav-link">Reservation History</div>
+                        <div className="nav-link" onClick={logoutNav}>Logout</div>
+                    </div>
                 </div>
             </div>
-            <ProfileModal 
+            <ProfileModal
                 show={showProfileModal}
                 onClose={closeProfileModal}
                 studentData={studentData}
