9df1f88c24981dc75557371fa8d7589a76e65d5b
weswilson24 <wwilson74@gatech.edu>
2024-10-31 16:09:21 -0400

    forgor to delete this line

--- frontend/src/connector.js
+++ frontend/src/connector.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-const BACKEND_URL2 = process.env.REACT_APP_BACKEND_URL2;
+
 
 const getStudents = async () => {
     try {
