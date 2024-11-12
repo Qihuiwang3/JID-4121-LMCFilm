5be7d7e581a523a953af99dea8d5e2b92f0d5aa9
denial <danpan1232@gmail.com>
2024-11-12 18:41:38 -0500

    Add Equipment update modal

--- frontend/src/components/Pages/Equipment/Equipment.js
+++ frontend/src/components/Pages/Equipment/Equipment.js
@@ -1,11 +1,13 @@
 import React, { useState, useRef } from 'react';
-import EquipmentPopup from '../../Modal/EquipmentPopup/EquipmentAddPopup';
-
+import EquipmentAddPopup from '../../Modal/EquipmentPopup/EquipmentAddPopup';
+import EquipmentUpdatePopup from '../../Modal/EquipmentPopup/EquipmentUpdatePopup'; // Import the update popup
 import BackButton from '../../Button/BackButton/BackButton';
 import EquipmentTable from '../../Functions/EquipmentTable/EquipmentTable';
 
 const Equipment = () => {
     const [showPopup, setShowPopup] = useState(false);
+    const [showUpdatePopup, setShowUpdatePopup] = useState(false); // State for update popup
+    const [selectedItem, setSelectedItem] = useState(null); // State to hold selected item data
 
     const [isEditMode, setIsEditMode] = useState(false);
     const equipmentTableRef = useRef(null);
@@ -25,6 +27,22 @@ const Equipment = () => {
         setShowPopup(false);
     };
 
+    const handleOpenUpdatePopup = (itemData) => {
+        setSelectedItem(itemData); // Set the selected item data to pass to the update modal
+        setShowUpdatePopup(true); // Show the update popup
+    };
+
+    const handleCloseUpdatePopup = () => {
+        setShowUpdatePopup(false);
+        setSelectedItem(null); // Clear selected item data when closing
+    };
+
+    const refreshTableData = () => {
+        if (equipmentTableRef.current) {
+            equipmentTableRef.current.loadRecords();
+        }
+    };
+
     return (
         <div>
             <EquipmentTable
@@ -32,6 +50,7 @@ const Equipment = () => {
                 isEditMode={isEditMode}
                 toggleEditMode={toggleEditMode}
                 handleOpenPopup={handleOpenPopup}
+                handleOpenUpdatePopup={handleOpenUpdatePopup} // Pass down to handle row icon click
             />
 
             <div className="equipment-btn">
@@ -40,9 +59,19 @@ const Equipment = () => {
                 </div>
             </div>
 
-            <EquipmentPopup show={showPopup} handleClose={handleClosePopup} />
+            {/* Render Add Popup */}
+            <EquipmentAddPopup show={showPopup} handleClose={handleClosePopup} />
+
+            {/* Render Update Popup with selected data */}
+            <EquipmentUpdatePopup
+                show={showUpdatePopup}
+                handleClose={handleCloseUpdatePopup}
+                existingData={selectedItem} // Pass the selected item data to the update popup
+                onUpdateComplete={refreshTableData} // Pass the callback function
+            />
         </div>
     );
 };
 
 export default Equipment;
+
