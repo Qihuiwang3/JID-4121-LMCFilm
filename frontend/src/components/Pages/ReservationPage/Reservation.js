81fb3180a41158896c559d01489d84725d8cbf66
weswilson24 <wwilson74@gatech.edu>
2024-11-22 15:47:28 -0500

    in place example

--- frontend/src/components/Pages/ReservationPage/Reservation.js
+++ frontend/src/components/Pages/ReservationPage/Reservation.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import TimeSelectionButton from '../../Button/TimeSelectionButton/TimeSelectionButton.js';
+import TimeSelectionButton from '../../Button/TimeSelectionButton/TimeSelectionButton.js';  // Keep the TimeSelectionButton
 import EquipmentDropdown from '../../Dropdown/EquipmentDropdown/EquipmentDropdown.js';
 import PackageDropdown from '../../Dropdown/PackageDropdown/PackageDropdown.js';
 import './Reservation.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { setSelectedDates, setClassCode } from '../../redux/actions/classActions';
 import { setReservationCartItems } from '../../redux/actions/reservationCartActions.js';
 import { createCartWithData, getSingleItemsByClassCode, getBundleItemsByClassCode, getItemByName } from '../../../connector.js';
@@ -17,43 +18,42 @@ function ReservationPage() {
     const classCode = useSelector(state => state.classData.classCode);
     const selectedDates = useSelector(state => state.classData.selectedDates);
     const reservationCartItems = useSelector(state => state.reservationCart.reservationCartItems);
-    const reduxStudentInfo = useSelector(state => state.studentData);
-
     const location = useLocation();
+    const reduxStudentInfo = useSelector(state => state.studentData);
     const studentInfo = location.state?.studentInfo || reduxStudentInfo;
 
     const [pickupDateTime, setPickupDateTime] = useState(new Date(selectedDates?.pickupDateTime || new Date()));
     const [returnDateTime, setReturnDateTime] = useState(new Date(selectedDates?.returnDateTime || new Date()));
+
     const [equipment, setEquipment] = useState([]);
     const [bundles, setBundles] = useState([]);
     const [cartItems, setCartItems] = useState([]);
 
+
     useEffect(() => {
         dispatch(setSelectedDates(pickupDateTime, returnDateTime));
         setCartItems(reservationCartItems);
-    }, [pickupDateTime, returnDateTime, dispatch, reservationCartItems]);
+    }, [pickupDateTime, returnDateTime, dispatch]);
 
     const addToCart = (item) => {
-        const itemId = item.itemId || item.bundleId;
-        const isItemInCart = cartItems.some(cartItem => (cartItem.itemId || cartItem.bundleId) === itemId);
-        if (!isItemInCart) {
+        if (!cartItems.includes(item)) {
             setCartItems([...cartItems, item]);
+        } else {
+            setCartItems(cartItems.filter(cartItems => cartItems !== item));
         }
-    };
-    
-
-    const removeFromCart = (item) => {
-        const itemId = item.itemId || item.bundleId;
-        setCartItems(cartItems.filter(cartItem => (cartItem.itemId || cartItem.bundleId) !== itemId));
     };
 
     const calculateTotal = () => {
         let total = 0;
-        if (studentInfo.role !== 'Admin' && studentInfo.role !== 'Professor') {
+
+        if (studentInfo.role === 'Admin' || studentInfo.role === 'Professor') {
+            total = 0
+        } else {
             cartItems.forEach(item => {
                 total += item.price;
             });
         }
+
         return total.toFixed(2);
     };
 
@@ -61,8 +61,8 @@ function ReservationPage() {
         const cartData = { cartItems };
         dispatch(setReservationCartItems(cartItems));
         try {
+            const response = await createCartWithData(cartData);
             navigate('/CartConfirmation', { state: { cartItems } });
-            console.log(cartItems);
         } catch (error) {
             console.error('Error creating cart:', error);
         }
@@ -77,9 +77,10 @@ function ReservationPage() {
         const fetchData = async () => {
             if (classCode) {
                 try {
+                    // Fetch single items
                     const singleItems = await getSingleItemsByClassCode(classCode);
                     const promises = singleItems.map(async (singleItem) => {
-                        const itemDetails = await getItemByName(singleItem.itemName);
+                        const itemDetails = await getItemByName(singleItem.itemName); // Updated
                         return {
                             ...singleItem,
                             pricePerItem: itemDetails.pricePerItem,
@@ -90,8 +91,10 @@ function ReservationPage() {
                     const equipmentWithPricesQuantity = await Promise.all(promises);
                     setEquipment(equipmentWithPricesQuantity);
 
+                    // Fetch bundles
                     const bundleItems = await getBundleItemsByClassCode(classCode);
                     setBundles(bundleItems);
+
                 } catch (error) {
                     console.error('Error fetching data:', error);
                 }
@@ -115,7 +118,7 @@ function ReservationPage() {
 
             <div className="equipment-and-cart">
                 <div className="equipment-container">
-                    {equipment && equipment.length > 0 && !equipment.every(item => item.quantity === 0 || item.quantity === undefined) && (
+                    {equipment && equipment.length > 0 && (
                         <EquipmentDropdown
                             id="equipment"
                             title="Available Equipment"
@@ -128,7 +131,6 @@ function ReservationPage() {
                             showReserve={true}
                             showQuantity={false}
                             addItem={addToCart}
-                            cartItems={cartItems}
                         />
                     )}
 
@@ -145,7 +147,6 @@ function ReservationPage() {
                             showReserve={true}
                             showQuantity={false}
                             addItem={addToCart}
-                            cartItems={cartItems}
                         />
                     )}
                 </div>
@@ -153,24 +154,21 @@ function ReservationPage() {
                 <div className="cart-container">
                     <h1 style={{ paddingLeft: "20px", color: "#3361AE" }}>Cart</h1>
                     <div className="all-cart-items">
-                        {cartItems.map(item => (
-                            <div key={item.itemId || item.bundleId}>
+                        {cartItems.map(id => (
+                            <div key={id.itemId || id.bundleId}>
                                 <div className="cart-item">
                                     <div>
                                         <div className="cart-first-row">
-                                            <div>{item.name}</div>
-                                            <div> ${item.price}</div>
+                                            <div>{id.name}</div>
+                                            <div> ${id.price}</div>
                                         </div>
                                         <div className="cart-second-row">
-                                            <div className="remove-equipment-item" onClick={() => removeFromCart(item)}>Remove</div>
+                                            <div className="remove-equipment-item" onClick={() => addToCart(id)}>Remove</div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         ))}
-                        {cartItems.length === 0 && (
-                            <div className="empty-cart">Your cart is empty.</div>
-                        )}
                     </div>
                     <div style={{ textAlign: "right", paddingRight: "20px" }}>Total: ${calculateTotal()}</div>
                 </div>
@@ -178,13 +176,14 @@ function ReservationPage() {
 
             <div className='btnContainer'>
                 <Button type="back" onClick={handleBack}>Back</Button>
-                {cartItems.length !== 0 && (
+                {/* Conditionally render the checkout button */}
+                {cartItems.length === 0 ? null : (
                     <Button type="next" onClick={handleCheckout}>
                         Checkout
                     </Button>
                 )}
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
 
